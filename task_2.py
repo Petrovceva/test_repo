ce259779780a9e3683696be582ed30bedd9b876c
Petrovceva <123365984+Petrovceva@users.noreply.github.com>
2023-12-03 20:56:23 +0300

    task_2.py

---
+++ task_2.py
@@ -0,0 +1,21 @@
+import subprocess
+import zlib
+
+
+def check_command(command, text):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = result.stdout
+    if result.returncode == 0 and text in out:
+        return True
+    else:
+        return False
+def get_command(command):
+    return subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+def get_hash(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+    return hex(zlib.crc32(data))
+
+if __name__=='__main__':
+    check_command('cd /home/user/folder_ex; touch file_1', "")
