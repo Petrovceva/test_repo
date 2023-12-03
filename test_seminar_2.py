06989c9008a4d19ad821a29a0ecf6ec2ca9cd134
Petrovceva <123365984+Petrovceva@users.noreply.github.com>
2023-12-03 20:58:05 +0300

    test_seminar_2.py

---
+++ test_seminar_2.py
@@ -0,0 +1,29 @@
+from task_2 import check_command, get_command
+import pytest
+import yaml
+
+with open('config.yaml', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+
+
+@pytest.mark.usefixtures('make_folders', 'make_files', 'delete_folders', 'print_time', 'stat')
+class TestSeminar:
+
+    def test_step_1(self):
+        assert check_command(f'cd {data.get('folder_in')}; 7z a {data.get('folder_out')}/archive_1', 'Everything is OK')
+
+    def test_step_2(self):
+        assert check_command(f"cd {data.get('folder_out')}; 7z rn archive_1 file_1.txt file_2.txt file_3.txt", 'Everything is OK')
+
+    def test_step_3(self):
+        assert check_command(f"cd {data.get('folder_out')}; 7z i archive_1", '0 ED 6F00181 AES256CBC')
+
+    def test_step_4(self):
+        output = str(get_hash(f'{data.get('folder_out')}/archive_1.7z')).upper()[2:]
+        assert check_command(f"cd {data.get('folder_out')}; 7z h archive_1.7z", output)
+
+    def test_step_5(self):
+        assert check_command(f"cd {data.get('folder_out')}; 7z x archive_1.7z", 'Everything is OK')
+
+if __name__=='__main__':
+    pytest.main(['-vv'])
