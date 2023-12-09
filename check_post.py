12a77956c913d4ab37723fcfe27e30db53a6111d
Petrovceva <123365984+Petrovceva@users.noreply.github.com>
2023-12-09 19:47:06 +0300

    check_post.py

---
+++ check_post.py
@@ -0,0 +1,32 @@
+import requests
+import yaml
+
+
+with open('config.yaml', 'r', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+
+def get_login():
+    path = data['path']
+    post = requests.post(url=path, data={'username': data['username'], 'password': data['password']})
+    if post.status_code == 200:
+        return post.json()['token']
+    
+
+def get_post(token):
+    path = 'https://test-stand.gb.ru/api/posts'
+    get = requests.get(url=path, param={'owner': 'notMe'}, headers={'X-Auth-Token': token})
+    if get.status_code == 200:
+        return get.json()
+    
+
+def get_newpost():
+    path = 'https://test-stand.gb.ru/api/posts'
+    post = requests.get(url=path, data={'title': data['title'], 'description': data['description'], 'content': data['content']})
+    if post.status_code == 200:
+        return post.json()['token']
+    
+
+if __name__=='__main__':
+    token = get_login()
+    print(get_post(token))
+    print(get_newpost(token))
